b1e23e7b34edebcafed8fbc699b043db6eb1337c
Holly <hollykvalheim@gmail.com>
2022-10-01 15:41:36 -0700

    commit tree dc5e186230590534c3d3afe379557dd2ddf8eb45

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+main = document.querySelector('main#main');
+main.remove();
+
+const newHeader = document.createElement('h1');
+const head = document.querySelector('head');
+/*document.head.append(newHeader);*/
+newHeader.id = "victory"
+
+newHeader.textContent = 'Gizzy is the champion'
